6b3d77a975a4b159d72aa59f0a5648a9e007019e
Sebastian Balderrama <capotillos11@gmail.com>
2024-11-18 22:33:38 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 #![allow(unused_imports)]
+use std::str::from_utf8;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener, TcpStream};
+
+enum RESP {
+    PING,
+    ECHO(String),
+}
 
 #[tokio::main]
 async fn main() {
@@ -8,18 +14,41 @@ async fn main() {
 
     loop {
         match listener.accept().await {
-            Ok((mut stream, _)) => {
-                tokio::spawn(async move {
-                    let mut buf = [0; 256];
-
-                    while stream.read(&mut buf).await.unwrap() > 0 {
-                        stream.write(b"+PONG\r\n").await.unwrap();
-                    }
-                });
+            Ok((stream, _)) => {
+                println!("Connection Received");
+                tokio::spawn(async move { handle_conn(stream).await });
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+            Err(e) => println!("error: {}", e),
         }
+    }
+}
+
+async fn handle_conn(mut stream: TcpStream) {
+    let mut buf = [0; 256];
+
+    loop {
+        let n = stream.read(&mut buf).await.unwrap();
+        if n == 0 {
+            break;
+        }
+        stream
+            .write(
+                match resp_tokenize(from_utf8(&buf[0..n]).unwrap()) {
+                    RESP::PING => "+PONG\r\n".to_string(),
+                    RESP::ECHO(msg) => format!("+{}\r\n", msg),
+                }
+                .as_bytes(),
+            )
+            .await
+            .unwrap();
+    }
+}
+
+fn resp_tokenize(resp: &str) -> RESP {
+    let args: Vec<&str> = resp.split("\r\n").collect();
+    match args[2] {
+        "PING" => RESP::PING,
+        "ECHO" => RESP::ECHO(args[4].to_string()),
+        _ => panic!("Unknown Command"),
     }
 }
