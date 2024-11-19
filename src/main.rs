85434f9061414a00686aecda62fc8f810d4eee5c
Sebastian Balderrama <capotillos11@gmail.com>
2024-11-18 23:59:13 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,42 @@
 #![allow(unused_imports)]
+use std::collections::HashMap;
 use std::str::from_utf8;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 
-enum RESP {
+enum CMD {
     PING,
     ECHO(String),
+    SET(String, String),
+    GET(String),
+}
+
+impl CMD {
+    pub fn response(self, env: &HashMap<String, String>) -> String {
+        match self {
+            CMD::PING => "+PONG\r\n".to_string(),
+            CMD::ECHO(msg) => format!("+{}\r\n", msg),
+            CMD::SET(..) => "+OK\r\n".to_string(),
+            CMD::GET(key) => match env.get(&key) {
+                Some(val) => format!("${}\r\n{}\r\n", val.len(), val),
+                None => "$-1\r\n".to_string(),
+            },
+        }
+    }
+
+    pub fn run(&self, env: &mut HashMap<String, String>) {
+        match self {
+            CMD::SET(key, value) => {
+                env.insert(key.clone(), value.clone());
+            }
+            _ => {}
+        };
+    }
 }
 
 #[tokio::main]
 async fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    let listener: TcpListener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
     loop {
         match listener.accept().await {
@@ -24,31 +50,27 @@ async fn main() {
 }
 
 async fn handle_conn(mut stream: TcpStream) {
-    let mut buf = [0; 256];
+    let mut buf: [u8; 256] = [0; 256];
+    let mut env: HashMap<String, String> = HashMap::new();
 
     loop {
-        let n = stream.read(&mut buf).await.unwrap();
-        if n == 0 {
+        let bytes_read: usize = stream.read(&mut buf).await.unwrap();
+        if bytes_read == 0 {
             break;
         }
-        stream
-            .write(
-                match resp_tokenize(from_utf8(&buf[0..n]).unwrap()) {
-                    RESP::PING => "+PONG\r\n".to_string(),
-                    RESP::ECHO(msg) => format!("+{}\r\n", msg),
-                }
-                .as_bytes(),
-            )
-            .await
-            .unwrap();
+        let cmd: CMD = resp_deserialize(from_utf8(&buf[..bytes_read]).unwrap());
+        cmd.run(&mut env);
+        stream.write(cmd.response(&env).as_bytes()).await.unwrap();
     }
 }
 
-fn resp_tokenize(resp: &str) -> RESP {
+fn resp_deserialize(resp: &str) -> CMD {
     let args: Vec<&str> = resp.split("\r\n").collect();
     match args[2] {
-        "PING" => RESP::PING,
-        "ECHO" => RESP::ECHO(args[4].to_string()),
+        "PING" => CMD::PING,
+        "ECHO" => CMD::ECHO(args[4].to_string()),
+        "SET" => CMD::SET(args[4].to_string(), args[6].to_string()),
+        "GET" => CMD::GET(args[4].to_string()),
         _ => panic!("Unknown Command"),
     }
 }
